88600bd580436b72c65a969abb91d809dd4b3de5
Yuheng Su <gipsyh.icu@gmail.com>
2024-02-21 17:48:46 +0800

    update

--- src/activity.rs
+++ src/activity.rs
@@ -1,25 +1,30 @@
 use logic_form::{Cube, Var, VarMap};
 
 pub struct Activity {
-    activity: VarMap<f64>,
+    activity: Vec<f64>,
+    idx_map: VarMap<usize>,
     act_inc: f64,
 }
 
 impl Activity {
     pub fn new(var: &[Var]) -> Self {
-        let mut activity = VarMap::new();
-        for v in var.iter() {
-            activity.reserve(*v);
+        let mut idx_map = VarMap::new();
+        let mut activity = Vec::new();
+        for i in 0..var.len() {
+            idx_map.reserve(var[i]);
+            idx_map[var[i]] = i;
+            activity.push(0.0);
         }
         Self {
             activity,
+            idx_map,
             act_inc: 1.0,
         }
     }
 
     #[inline]
     fn bump(&mut self, var: Var) {
-        self.activity[var] += self.act_inc;
+        self.activity[self.idx_map[var]] += self.act_inc;
     }
 
     #[inline]
@@ -36,15 +41,23 @@ impl Activity {
 
     pub fn sort_by_activity(&self, cube: &mut Cube, ascending: bool) {
         if ascending {
-            cube.sort_by(|a, b| self.activity[*a].partial_cmp(&self.activity[*b]).unwrap());
+            cube.sort_by(|a, b| {
+                self.activity[self.idx_map[*a]]
+                    .partial_cmp(&self.activity[self.idx_map[*b]])
+                    .unwrap()
+            });
         } else {
-            cube.sort_by(|a, b| self.activity[*b].partial_cmp(&self.activity[*a]).unwrap());
+            cube.sort_by(|a, b| {
+                self.activity[self.idx_map[*b]]
+                    .partial_cmp(&self.activity[self.idx_map[*a]])
+                    .unwrap()
+            });
         }
     }
 
     #[allow(unused)]
     pub fn cube_average_activity(&self, cube: &Cube) -> f64 {
-        let sum: f64 = cube.iter().map(|l| self.activity[*l]).sum();
+        let sum: f64 = cube.iter().map(|l| self.activity[self.idx_map[*l]]).sum();
         sum / cube.len() as f64
     }
 }
