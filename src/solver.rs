8e665d369f6e644b9d9e8f238bbe458013c4e7ca
Yuheng Su <gipsyh.icu@gmail.com>
2024-03-24 11:30:10 +0800

    update

--- src/solver.rs
+++ src/solver.rs
@@ -1,6 +1,7 @@
 use crate::Ic3;
 use gipsat::{BlockResult, BlockResultNo};
 use logic_form::Cube;
+use satif::SatifSat;
 
 impl Ic3 {
     pub fn blocked_with_ordered(
@@ -20,6 +21,16 @@ impl Ic3 {
 
 impl Ic3 {
     pub fn unblocked_model(&mut self, unblock: BlockResultNo) -> Cube {
-        self.gipsat.minimal_predecessor(unblock)
+        let mut latchs = Cube::new();
+        for latch in self.ts.latchs.iter() {
+            let lit = latch.lit();
+            match unblock.sat.lit_value(lit) {
+                Some(true) => latchs.push(lit),
+                Some(false) => latchs.push(!lit),
+                None => (),
+            }
+        }
+        self.activity.sort_by_activity(&mut latchs, false);
+        self.gipsat.minimal_predecessor(unblock, latchs)
     }
 }
