5d86f7ff7fb81ff3c20332102b7e9b9dfa046221
Yuheng Su <gipsyh.icu@gmail.com>
2024-05-21 20:30:52 +0800

    update

--- src/activity.rs
+++ src/activity.rs
@@ -1,4 +1,6 @@
-use logic_form::{Cube, Var, VarMap};
+use crate::IC3;
+use logic_form::{Cube, Lit, Var, VarMap};
+use std::collections::HashMap;
 
 pub struct Activity {
     activity: Vec<f64>,
@@ -40,18 +42,15 @@ impl Activity {
     }
 
     pub fn sort_by_activity(&self, cube: &mut Cube, ascending: bool) {
+        let ascending_func = |a: &Lit, b: &Lit| {
+            self.activity[self.idx_map[*a]]
+                .partial_cmp(&self.activity[self.idx_map[*b]])
+                .unwrap()
+        };
         if ascending {
-            cube.sort_by(|a, b| {
-                self.activity[self.idx_map[*a]]
-                    .partial_cmp(&self.activity[self.idx_map[*b]])
-                    .unwrap()
-            });
+            cube.sort_by(ascending_func);
         } else {
-            cube.sort_by(|a, b| {
-                self.activity[self.idx_map[*b]]
-                    .partial_cmp(&self.activity[self.idx_map[*a]])
-                    .unwrap()
-            });
+            cube.sort_by(|a, b| ascending_func(b, a));
         }
     }
 
@@ -59,5 +58,42 @@ impl Activity {
     pub fn cube_average_activity(&self, cube: &Cube) -> f64 {
         let sum: f64 = cube.iter().map(|l| self.activity[self.idx_map[*l]]).sum();
         sum / cube.len() as f64
+    }
+}
+
+impl IC3 {
+    pub fn sort_by_group_activity(&self, cube: &mut Cube, ascending: bool) {
+        let mut group = HashMap::new();
+        for i in 0..cube.len() {
+            let g = self.ts.latch_group[cube[i]];
+            if group.contains_key(&g) {
+                continue;
+            }
+            let mut num = 0;
+            let mut sum = 0.0;
+            for j in i..cube.len() {
+                if self.ts.latch_group[cube[j].var()] == g {
+                    num += 1;
+                    sum += self.activity.activity[self.activity.idx_map[cube[j]]];
+                }
+            }
+            group.insert(g, sum / num as f64);
+        }
+        let ascending_func = |a: &Lit, b: &Lit| {
+            if self.ts.latch_group[*a] == self.ts.latch_group[*b] {
+                self.activity.activity[self.activity.idx_map[*a]]
+                    .partial_cmp(&self.activity.activity[self.activity.idx_map[*b]])
+                    .unwrap()
+            } else {
+                group[&self.ts.latch_group[*a]]
+                    .partial_cmp(&group[&self.ts.latch_group[*b]])
+                    .unwrap()
+            }
+        };
+        if ascending {
+            cube.sort_by(ascending_func);
+        } else {
+            cube.sort_by(|a, b| ascending_func(b, a));
+        }
     }
 }
