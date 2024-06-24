9229ad7a8bb538f8909396d7b1c6b10fbb12b293
Yuheng Su <gipsyh.icu@gmail.com>
2024-06-24 10:59:01 +0800

    update

--- src/bmc.rs
+++ src/bmc.rs
@@ -1,6 +1,6 @@
 use crate::Args;
 use aig::Aig;
-use cadical::Solver;
+use kissat::Solver;
 use satif::Satif;
 use transys::{Transys, TransysUnroll};
 
@@ -35,11 +35,53 @@ impl BMC {
             match self.solver.solve(&[bad]) {
                 satif::SatResult::Sat(_) => {
                     println!("bmc found cex in depth {k}");
-                    return false;
+                    return true;
                 }
                 satif::SatResult::Unsat(_) => (),
             }
         }
         unreachable!();
+    }
+
+    pub fn check_in_depth(&mut self, depth: usize) -> bool {
+        println!("{}", self.args.model);
+        self.uts.ts.load_init(&mut self.solver);
+        for k in 0..=depth {
+            self.uts.unroll_to(k);
+            self.uts.load_trans(&mut self.solver, k);
+            if k != depth {
+                continue;
+            }
+            if self.args.verbose {
+                println!("bmc depth: {k}");
+            }
+            let bad = self.uts.lit_next(self.uts.ts.bad, k);
+            self.solver.add_clause(&[bad]);
+            match self.solver.solve(&[]) {
+                satif::SatResult::Sat(_) => {
+                    println!("bmc found cex in depth {k}");
+                    return true;
+                }
+                satif::SatResult::Unsat(_) => (),
+            }
+        }
+        false
+    }
+
+    pub fn check_no_incremental(&mut self) -> bool {
+        if self.check_in_depth(70) {
+            return true;
+        }
+        if self.check_in_depth(130) {
+            return true;
+        }
+        for k in 140.. {
+            if k % 10 == 0 {
+                if self.check_in_depth(k) {
+                    return true;
+                }
+            }
+        }
+        unreachable!()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let args = Args::parse();
     if args.bmc {
         let mut bmc = BMC::new(args);
-        println!("bmc result: {}", bmc.check());
+        println!("bmc result: {}", !bmc.check_no_incremental());
     } else {
         let mut ic3 = IC3::new(args);
         println!("ic3 result: {}", ic3.check_with_int_hanlder());
